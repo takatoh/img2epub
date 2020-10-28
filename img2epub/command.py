6858aa48d4c703576d2bc7cadf8145caeff270dd
takatoh <takatoh.m@gmail.com>
2020-10-28 14:53:40 +0900

    img2epub/command.py: Use click, and add 'build' sub-command.

--- img2epub/command.py
+++ img2epub/command.py
@@ -1,13 +1,27 @@
 # encoding: utf-8
 
 
-import sys
+#import sys
+import click
 from datetime import datetime, timezone
 from img2epub import functions as func
 
 
 def main():
-    src_dir = sys.argv[1]
+    cmd(obj={})
+
+
+@click.group()
+@click.pass_context
+def cmd(ctx):
+    pass
+
+
+@cmd.command(help="Build EPUB book.")
+@click.pass_context
+@click.argument("src_dir")
+def build(ctx, src_dir):
+#    src_dir = sys.argv[1]
     now = datetime.now(timezone.utc)
     tmp_dir_name = "tmp.epub.{time}".format(time=now.strftime("%Y%m%d%H%M%S"))
 
